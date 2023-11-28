03b47ba9728a37e72b0f2121211f6913ad708b14
Prabhatkumar199 <152252898+Prabhatkumar199@users.noreply.github.com>
2023-11-28 08:09:06 +0000

    Datatype

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 144553
+let accountEmail ="prabhat@google.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+let accountstate;
+
+//accountId = 2
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountstate]);
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) //we are using nodejs, not browser
+
+console.log(3+3) //code readability should be high
+
+console.log("prabhat")
+
+let name ="prabhat"
+let age = 18
+let isLooggedIn =false
+
+//number =>2 to power 53
+//bigint
+//string =>true/false
+//null =>standalone value
+//undefiend=>
+//symbol=>unique
+
+//object
+console.log(typeof null);
