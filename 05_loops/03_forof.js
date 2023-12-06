400a05648f1f080ee4bf538520e2f7dc86333a30
Prabhatkumar199 <152252898+Prabhatkumar199@users.noreply.github.com>
2023-12-06 09:41:47 +0000

    forEach loops

---
+++ 05_loops/03_forof.js
@@ -0,0 +1,63 @@
+// [{},{},{}]
+
+const arr = [1,2,3,4,5,6]
+
+for (const num of arr) {
+    // console.log(num);
+    
+}
+const greetings ="hello world"
+for (const greet of greetings) {
+    // console.log(`Each chart is ${greet}`);
+}
+
+//***************Maps************ */
+
+// const map = new Map()
+// map.set('IN',"INDIA")
+// map.set('UK',"UNITED KINDOM")
+// map.set('FC',"FRANCE")
+// map.set('AF',"AFCIKA")
+
+//console.log(map);
+
+//for (const [key,value] of map) {
+   // console.log(key,':-',value);
+//}
+
+const myobject ={
+    'game1':'NFS',
+    'game2':'spiderman'
+}
+
+// for (const [key,value] of myobject) {
+//     console.log(key,':-',value);
+// }
+
+const myObject = {
+    js:'javascript',
+    cpp:'C++',
+    RB:'ruby',
+    swift:'swift by apple'
+}
+for (const key in myObject) {
+//    console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programing =["js","rb","py","java","cpp"]
+
+for (const key in programing) {
+//    console.log(programing[key]);
+}
+
+
+// const map = new Map()
+// map.set('IN',"INDIA")
+// map.set('UK',"UNITED KINDOM")
+// map.set('FC',"FRANCE")
+// map.set('AF',"AFCIKA")
+
+// for (const key in map) {
+//     console.log(key);
+// }
+
